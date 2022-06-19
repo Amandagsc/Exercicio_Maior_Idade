581e575db23b3fcdb1d2ff5b51a753fc0684b168
Amandagsc <70444717+Amandagsc@users.noreply.github.com>
2022-06-19 08:20:31 -0300

    first commit

---
+++ Exercicio_Maior_Idade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio_Maior_Idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
