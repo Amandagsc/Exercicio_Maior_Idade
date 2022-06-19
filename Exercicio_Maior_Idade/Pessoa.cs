83070f7b622ae9358c633fbc9443346590d514e5
Amandagsc <70444717+Amandagsc@users.noreply.github.com>
2022-06-19 10:16:32 -0300

    Adição de novoa arquivos

---
+++ Exercicio_Maior_Idade/Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercicio_Maior_Idade
+{
+    public class Pessoa
+    {
+        //Construtores
+        public Pessoa()
+        {
+            this.Nome = "";
+            this.Idade = 0;
+        }
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+        //Propriedades
+        private string nome;
+        private string Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        public int idade;
+        public int Idade
+        {
+            get { return idade; }
+            set
+            {
+                if (value >= 0)
+                {
+                    idade = value;
+                }
+                else
+                {
+                    this.idade = 0;
+                }
+            }
+        }
+
+        //Métodos
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Idade: " + this.Idade);
+        }
+    }
+}
--- Exercicio_Maior_Idade/Program.cs
+++ Exercicio_Maior_Idade/Program.cs
@@ -6,7 +6,54 @@ namespace Exercicio_Maior_Idade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+            Pessoa p3 = new Pessoa();
+
+            Console.WriteLine("Determinando a Maior Idade");
+
+            //coletando Dados
+            Console.WriteLine("Digite o nome da 1ª pessoa: ");
+            Console.ReadLine();
+            Console.WriteLine("Digite a idade da 1ª pessoa: ");
+            p1 .idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da 2ª pessoa: ");
+            Console.ReadLine();
+            Console.WriteLine("Digite a idade da 2ª pessoa: ");
+            p2.idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da 3ª pessoa: ");
+            Console.ReadLine();
+            Console.WriteLine("Digite a idade da 3ª pessoa: ");
+            p3.idade = Convert.ToInt32(Console.ReadLine());
+
+            //Lógica
+            if ((p1.idade > p2.idade) && (p1.idade > p3.idade))
+            {
+                Console.WriteLine("A pessoa com maior idade é: ");
+                p1.ExibirDados();
+            }
+            else
+            {
+                if ((p2.idade >p1.idade) && (p2.idade> p3.idade))
+                {
+                    Console.WriteLine("A pessoa com maior idade é: ");
+                    p2.ExibirDados();
+                }
+                else
+                {
+                    if ((p3.idade > p1.idade) && (p3.idade > p2.idade))
+                    {
+                        Console.WriteLine("A pessoa com maior idade é: " );
+                        p3.ExibirDados();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Todas as pessoas Possuem a mesma idade! ");
+                    }
+                }
+            }
         }
     }
 }
